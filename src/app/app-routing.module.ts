172d0ea8c36b472bac156d5d590929becef7bfd6
vahan.martirosyan <vahan.martirosyan@connectto.com>
2023-01-25 21:00:29 +0400

    ad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AllPortfolioComponent} from "./ad-agency/all-portfolio/all-portfolio.component";
-import {GraphicPortfolioComponent} from "./ad-agency/graphic-portfolio/graphic-portfolio.component";
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AllPortfolioComponent } from './ad-agency/all-portfolio/all-portfolio.component';
-import { WebPortfolioComponent } from './ad-agency/web-portfolio/web-portfolio.component';
-import { SeoPortfolioComponent } from './ad-agency/seo-portfolio/seo-portfolio.component';
-import { MarketingPortfolioComponent } from './ad-agency/marketing-portfolio/marketing-portfolio.component';
 import {AdAgencyComponent} from "./ad-agency/ad-agency.component";
-import {GraphicPortfolioComponent} from "./ad-agency/graphic-portfolio/graphic-portfolio.component";
+import {PortfolioServiceCarouselComponent} from "./carousel_portfolio_startup/portfolio-service-carousel.component";
+import {CarouselModule} from "ngx-owl-carousel-o";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllPortfolioComponent,
-    WebPortfolioComponent,
-    SeoPortfolioComponent,
-    MarketingPortfolioComponent,
     AdAgencyComponent,
-    GraphicPortfolioComponent
+    PortfolioServiceCarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

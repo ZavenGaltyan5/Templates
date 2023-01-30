8692b9e36449ec779556acb2e9607926ee48c2c6
vahan.martirosyan <vahan.martirosyan@connectto.com>
2023-01-30 09:50:48 +0400

    ad

--- src/app/ad-agency/ad-agency.component.ts
+++ src/app/ad-agency/ad-agency.component.ts
@@ -28,7 +28,6 @@ export class AdAgencyComponent {
     div1.classList.remove('d-none')
   }
 
-
   Portfolio2(){
     let div1 = document.getElementById('pills-home') as HTMLElement;
     let div2 = document.getElementById('pills-profile') as HTMLElement;
@@ -42,7 +41,6 @@ export class AdAgencyComponent {
     div5.classList.add('d-none')
     div2.classList.remove('d-none')
   }
-
 
   Portfolio3(){
     let div1 = document.getElementById('pills-home') as HTMLElement;
@@ -58,8 +56,6 @@ export class AdAgencyComponent {
     div3.classList.remove('d-none')
   }
 
-
-
   Portfolio4(){
     let div1 = document.getElementById('pills-home') as HTMLElement;
     let div2 = document.getElementById('pills-profile') as HTMLElement;
@@ -74,9 +70,6 @@ export class AdAgencyComponent {
     div4.classList.remove('d-none')
   }
 
-
-
-
   Portfolio5(){
     let div1 = document.getElementById('pills-home') as HTMLElement;
     let div2 = document.getElementById('pills-profile') as HTMLElement;
@@ -90,6 +83,180 @@ export class AdAgencyComponent {
     div3.classList.add('d-none')
     div5.classList.remove('d-none')
   }
+
+
+  Scroll() {
+    window.onscroll = function () {
+      myFunction()
+    };
+    var header = document.getElementById("myHeader") as HTMLElement;
+    // @ts-ignore
+    var sticky = header.offsetTop;
+    var headCont = document.getElementById('head-cont') as HTMLElement;
+
+    var headLogo = document.getElementById('hidden-logo') as HTMLElement;
+    var headBtn = document.getElementById('hidden-btn') as HTMLElement;
+
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        headCont.classList.add('pb-b')
+        headLogo.classList.add('d-block')
+        headBtn.classList.add('d-block')
+      } else {
+        header.classList.remove("sticky");
+        headCont.classList.remove('pb-b')
+        headLogo.classList.remove('d-block')
+        headBtn.classList.remove('d-block')
+      }
+    }
+  }
+
+  carClick1(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci1.classList.add('active')
+    ci2.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick2(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci2.classList.add('active')
+    ci1.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick3(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci3.classList.add('active')
+    ci2.classList.remove('active')
+    ci1.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick4(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci4.classList.add('active')
+    ci2.classList.remove('active')
+    ci1.classList.remove('active')
+    ci3.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick5(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci5.classList.add('active')
+    ci2.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci1.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick6(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci6.classList.add('active')
+    ci2.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci1.classList.remove('active')
+    ci7.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick7(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci7.classList.add('active')
+    ci2.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci1.classList.remove('active')
+    ci8.classList.remove('active')
+  }
+  carClick8(){
+    let ci1 = document.getElementById('CI1') as HTMLElement;
+    let ci2 = document.getElementById('CI2') as HTMLElement;
+    let ci3 = document.getElementById('CI3') as HTMLElement;
+    let ci4 = document.getElementById('CI4') as HTMLElement;
+    let ci5 = document.getElementById('CI5') as HTMLElement;
+    let ci6 = document.getElementById('CI6') as HTMLElement;
+    let ci7 = document.getElementById('CI7') as HTMLElement;
+    let ci8 = document.getElementById('CI8') as HTMLElement;
+    ci8.classList.add('active')
+    ci2.classList.remove('active')
+    ci3.classList.remove('active')
+    ci4.classList.remove('active')
+    ci5.classList.remove('active')
+    ci6.classList.remove('active')
+    ci7.classList.remove('active')
+    ci1.classList.remove('active')
+  }
+
 
 
 
--- src/app/carousel_portfolio_startup/portfolio-service-carousel.component.ts
+++ src/app/carousel_portfolio_startup/portfolio-service-carousel.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-portfolio-service-carousel',
@@ -14,37 +13,4 @@ export class PortfolioServiceCarouselComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: false,
-    dotsSpeed: !1,
-    navSpeed: 3000,
-    margin: 30,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    autoplaySpeed: !1,
-    // navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      },
-      1920: {
-        items: 1
-      }
-    },
-    nav: false,
-  }
 }
